97f6b2594edb0fef79313d2b83bfa2676db6fc71
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-08-10 16:13:55 +0200

    e client

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,8 +1,8 @@
 const createError = require("http-errors");
 const Client = require("../models/client.model");
-const { lookup } = require('geoip-lite');
+// const { lookup } = require('geoip-lite');
 const ipapi = require('ipapi.co');
-const requestIp = require('request-ip');
+// const requestIp = require('request-ip');
 
 
 
@@ -38,28 +38,28 @@ module.exports.doConnected = (req, res, next) => {
 
 
 
-module.exports.doUpConnected = (req, res, next) => {
-  let query = req.body;
-  if (req.body.ipify) {
-    const ip = req.body.ipify.ip
-    const geoip = lookup(ip);
-    query.geoip = geoip
-  }
-  const id = req.body.id;
-  Client.findByIdAndUpdate(id, { $set: query }, { new: true })
-    .then((p) => {
-      if (p === null) {
-        next(createError(404, "the client could not be updated"));
-      } else {
-        res.status(201).json('ok');
-      }
-    })
-    .catch((e) => {
-      next(e);
-    });
-};
+// module.exports.doUpConnected = (req, res, next) => {
+//   let query = req.body;
+//   if (req.body.ipify) {
+//     const ip = req.body.ipify.ip
+//     const geoip = lookup(ip);
+//     query.geoip = geoip
+//   }
+//   const id = req.body.id;
+//   Client.findByIdAndUpdate(id, { $set: query }, { new: true })
+//     .then((p) => {
+//       if (p === null) {
+//         next(createError(404, "the client could not be updated"));
+//       } else {
+//         res.status(201).json('ok');
+//       }
+//     })
+//     .catch((e) => {
+//       next(e);
+//     });
+// };
 
-module.exports.doDisconnected = (req, res, next) => {
-  res.json('doDisconnected ok');
-};
+// module.exports.doDisconnected = (req, res, next) => {
+//   res.json('doDisconnected ok');
+// };
 
